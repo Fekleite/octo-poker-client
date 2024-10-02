edc8ad4d15b074784744189415c6bf89eb126695
Fernanda Leite <dev.fernandaleite@gmail.com>
2024-10-02 15:16:27 -0300

    feat: add connection and disconnection handlers

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,18 @@
+import { useEffect } from "react"
+import { socket } from "./socket"
+
 export function App() {
+  useEffect(() => {
+    socket.on('connect', () => {})
+
+    return () => {
+      socket.on('disconnect', () => {})
+    }
+  }, [])
+
   return (
     <div>
-      Hello, octo
+      Hello, octo!
     </div>
   )
 }
