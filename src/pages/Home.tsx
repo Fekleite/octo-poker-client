fd4bb65195674d4a0cb2b46a2ddc0aa4b3e875b7
Fernanda Leite <dev.fernandaleite@gmail.com>
2024-10-03 17:56:04 -0300

    feat: create home page

---
+++ src/pages/Home.tsx
@@ -0,0 +1,22 @@
+import octopus from '@/assets/octopus.png'
+import { Link } from 'react-router-dom'
+
+export function Home() {
+  return (
+    <div className="w-full h-screen bg-blue-500 flex justify-center items-center">
+      <div className='flex items-center gap-8'>
+        <img src={octopus} alt="Octopus" className='animate-fade-in'/>
+
+        <div className='animate-fade-in-up'>
+          <h1 className="font-display text-6xl text-slate-50">OctoPoker</h1>
+          <p className='text-slate-50 mt-2'>Collaborate with your team, plan what to do.</p>
+
+          <div className='flex items-center gap-2 mt-4'>
+            <Link to="/auth" className='inline-block px-4 py-2 rounded-md bg-pink-500 hover:bg-pink-600 duration-300 text-slate-50 font-medium'>Start a new room</Link>
+            <Link to="/auth" className='px-4 py-2 text-slate-50 font-medium'>Join in a room</Link>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
