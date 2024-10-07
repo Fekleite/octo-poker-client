52ac9f44c14ece74ec2e1627511e10f4c4c81d6b
Fernanda Leite <dev.fernandaleite@gmail.com>
2024-10-07 15:23:56 -0300

    feat: add create room handler

--- src/pages/CreateRoom.tsx
+++ src/pages/CreateRoom.tsx
@@ -1,7 +1,37 @@
+import { nanoid } from 'nanoid';
+import { useForm } from 'react-hook-form'
+import { Link, useNavigate } from 'react-router-dom'
+
 import octopus from '@/assets/octopus.png'
-import { Link } from 'react-router-dom'
+
+import { socket } from '@/socket';
+interface FormData {
+  username: string;
+  roomName: string;
+}
 
 export function CreateRoom() {
+  const { register, handleSubmit } = useForm<FormData>()
+  const navigate = useNavigate()
+
+  function handleCreateRoom(data: FormData) {
+    const roomCode = nanoid()
+    const payload = {
+      username: data.username,
+      roomCode,
+    }
+
+    localStorage.setItem("octopoker@data", JSON.stringify({
+      ...data,
+      roomCode,
+      role: "user-admin"
+    }))
+
+    socket.emit("join-room", payload);
+
+    navigate(`/room/join/${roomCode}`)
+  }
+
   return (
     <div className='w-full h-screen overflow-hidden flex'>
       <div className='w-3/5 h-full bg-blue-500 flex items-center justify-center'>
@@ -16,9 +46,22 @@ export function CreateRoom() {
       <div className='h-full flex-1 flex flex-col items-center justify-center animate-fade-in-up'>
        <h1 className="font-display text-6xl text-blue-500">OctoPoker</h1>
 
-        <form className='my-6 w-full max-w-60 flex flex-col gap-2'>
-          <input className='w-full h-10 bg-slate-100 px-4 py-2 rounded-md focus:ring-2 ring-blue-500 outline-none' type="text" placeholder='Type your name' />
-          <input className='w-full h-10 bg-slate-100 px-4 py-2 rounded-md focus:ring-2 ring-blue-500 outline-none' type="text" placeholder='Type the room name' />
+        <form 
+          onSubmit={handleSubmit(handleCreateRoom)}
+          className='my-6 w-full max-w-60 flex flex-col gap-2'
+        >
+          <input 
+            {...register('username')}
+            className='w-full h-10 bg-slate-100 px-4 py-2 rounded-md focus:ring-2 ring-blue-500 outline-none' 
+            type="text" 
+            placeholder='Type your name' 
+          />
+          <input
+            {...register('roomName')}
+            className='w-full h-10 bg-slate-100 px-4 py-2 rounded-md focus:ring-2 ring-blue-500 outline-none' 
+            type="text" 
+            placeholder='Type the room name' 
+          />
 
           <button type="submit" className='px-4 py-2 rounded-md bg-pink-500 hover:bg-pink-600 duration-300 text-slate-50 font-medium'>Create room</button>
         </form>
